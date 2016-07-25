da5aba9f87526ac3793187c2811070b0667db8ff
Ilya Baryshev <baryshev@gmail.com>
2016-07-25 11:06:23 +0300

    AWS_STORAGE_BUCKET_NAME from env

--- python_ru/settings.py
+++ python_ru/settings.py
@@ -147,4 +147,4 @@ if os.environ.get('AWS_ACCESS_KEY_ID'):
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'python-ru'
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')

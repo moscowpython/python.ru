978b4ef8eb545652280ce2548abc9a8adb660f97
Ilya Baryshev <baryshev@gmail.com>
2016-07-25 08:59:51 +0300

    Use s3boto

--- python_ru/settings.py
+++ python_ru/settings.py
@@ -144,7 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 if os.environ.get('AWS_ACCESS_KEY_ID'):
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'python-ru'

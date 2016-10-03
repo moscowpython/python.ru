a0e9545268ddbe7e661b501bc98c3ce298c746ec
Ilya Baryshev <baryshev@gmail.com>
2016-10-03 08:39:42 +0300

    use readability api to fetch lead image

--- apps/news/management/commands/pythondigest.py
+++ apps/news/management/commands/pythondigest.py
@@ -1,6 +1,7 @@
 import datetime
 import hashlib
 import logging
+import os
 
 import bleach
 import feedparser
@@ -11,6 +12,7 @@ from django.core.management.base import BaseCommand
 from django.db import IntegrityError
 from django.utils.timezone import make_aware
 from lxml.html import fromstring
+from readability import ParserClient
 
 from apps.news.models import Article
 
@@ -62,6 +64,18 @@ class Command(BaseCommand):
                 logger.info(self.style.SUCCESS('Fetched article "%s"' % article.name))
 
 
+def download_image(url):
+    if not url:
+        return
+    logger.info('Fetching image %s', url)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.content
+    except requests.RequestException as e:
+        logger.exception('Failed to fetch image %s', url)
+
+
 def fetch_image_from_summary(html):
     image_bytes = None
     if not html:
@@ -71,17 +85,23 @@ def fetch_image_from_summary(html):
         src = img.get('srcset')
         if not src:
             continue
-        try:
-            response = requests.get('https://pythondigest.ru/' + src)
-            response.raise_for_status()
-            image_bytes = response.content
-        except requests.RequestException as e:
-            pass
+        image_bytes = download_image('https://pythondigest.ru/' + src)
     return image_bytes, html
 
 
 def clean_html(html):
     return bleach.clean(html, tags=['p', 'a'], strip=True)
+
+
+def get_page_metadata(url):
+    token = os.environ.get('READABILITY_PARSER_KEY', None)
+    if not token:
+        return
+    try:
+        parser_client = ParserClient(token=os.environ.get('READABILITY_PARSER_KEY'))
+        return parser_client.get_article(url).json()
+    except Exception:
+        logger.exception('Failed to readability for url %s', url)
 
 
 def pydigest_article_feed():
@@ -109,6 +129,13 @@ def pydigest_articles_for_date(date):
 
     for item in response.json()['items']:
         image_bytes, summary = fetch_image_from_summary(item['description'])
+        extra_metadata = get_page_metadata(item['link'])
+
+        # fall back to readability data
+        summary = clean_html(summary) or clean_html(extra_metadata['excerpt'])
+        # fetch lead image from source
+        image_bytes = image_bytes or download_image(extra_metadata['lead_image_url'])
+
         yield dict(
             url=item['link'],
             name=item['title'],

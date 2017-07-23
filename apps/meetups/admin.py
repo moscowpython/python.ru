54136894518d810c0deb916c55a4d99525298b80
melevir <melevir@gmail.com>
2017-07-23 21:50:55 +0300

    remove old version

--- apps/meetups/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-
-admin.site.register(models.Employer)
-admin.site.register(models.Speaker)
-admin.site.register(models.Talk)
--- apps/meetups/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-
-urlpatterns = [
-    url(r'^(?P<event_slug>\w+)/$', views.EventDetailView.as_view(), name='event_detail_view'),
-]

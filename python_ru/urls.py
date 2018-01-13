cde1e8e53fbe069eb541e07faff403c9587f5621
a.shtyrnyaev <a.shtyrnyaev@finxplus.ru>
2018-01-13 13:10:25 +0300

    first commit

--- python_ru/urls.py
+++ python_ru/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^$', news_views.IndexView.as_view(), name='index'),
     url(r'^meetups/', include('apps.meetups.urls')),
     url(r'^junior/$', news_views.JuniorView.as_view(), name='junior'),
+    url(r'^post/(?P<pk>\d+)/$', news_views.PostView.as_view(), name='post_page'),
 
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

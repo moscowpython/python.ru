62f398e76b7a776a544eb7baaf4d8084b68a67f6
morgangt <morgangso@gmail.com>
2018-06-30 18:05:18 +0300

    update!

--- python_ru/settings/base.py
+++ python_ru/settings/base.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 # if not sys.argv[0].endswith('py.test'):
 #    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
 MEDIA_URL = '/media/'
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
